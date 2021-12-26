fc7cf10cde01a6349ced0c81a51c45690e3666d7
QSONG <qsong.contractor@libertyglobal.com>
2021-12-26 22:36:36 +0100

    add message provider

--- src/game_provider/huashan.py
+++ src/game_provider/huashan.py
@@ -10,7 +10,7 @@ class gao_message(object):
     '''
     This module provides gao008 functions
     '''
-    game_base_url = "http://www.gao008.com/So.asp"
+    game_base_url = "https://bbs.hszqb2.com/bbs-1.html"
 
     def __init__(self):
         '''
@@ -25,18 +25,20 @@ class gao_message(object):
         gao_response = requests.get(self.game_base_url)
         gao_response = gao_response
         if gao_response.status_code == 200:
-            gao_file = open('../gao888.html', 'wb')
-            gao_file.write(gao_response.content)
-            gao_file.close()
+            # gao_file = open('../gao888.html', 'wb')
+            # gao_file.write(gao_response.content)
+            # gao_file.close()
             soup = BeautifulSoup(gao_response.content, 'html.parser', from_encoding="gb18030")
             soup.prettify()
-            tr_list = soup.select('div[class*="trcolor"]')
+            tr_list = soup.select('ul')
 
-            for tr_elem in tr_list:
-                tr_elem_str = tr_elem.text
-                message_content_str = tr_elem_str.split("【")[0]
-                message_provider = tr_elem_str.split("【")[-1].split("】")[0]
-                message_time = tr_elem_str.split("【")[-1].split("】")[1]
+            tr_elem_str = tr_list[0]
+            gao_file = open('../../hslj.html', 'wb')
+            gao_file.write(tr_elem_str.contents)
+            gao_file.close()
+            message_content_str = tr_elem_str.split("【")[0]
+            message_provider = tr_elem_str.split("【")[-1].split("】")[0]
+            message_time = tr_elem_str.split("【")[-1].split("】")[1]
 
         return message_content_str, message_provider, message_time
 
